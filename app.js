0c12f887934ab0c2b759222f39d5518e8f02bf1a
elmaymayssa <68469614+elmaymayssa@users.noreply.github.com>
2020-07-18 23:33:56 +0100

    updated js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-if ('serviceWorker' in navigator) {
+ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js')
         .then((reg) => {
           console.log('Service worker registered.', reg);
-        });
-  });
+        })
+        .catch((err) => {
+			console.log('Unable to register service worker.', err);
+		});
+	});
 }
