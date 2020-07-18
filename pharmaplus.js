91d324e7cf29ebe183220df8bea3dbd70a506bca
elmaymayssa <68469614+elmaymayssa@users.noreply.github.com>
2020-07-18 23:26:44 +0100

    fixed js

--- pharmaplus.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-    }, function(err) {
-    // registration failed :(
-    console.log('ServiceWorker registration failed: ', err);
-    });
-    });
-    }
